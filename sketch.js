ad6d62b7d705067edc337c2082ba1cfc744a3b9d
candy441 <80819337+candy441@users.noreply.github.com>
2021-03-17 17:45:08 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://candy441.github.io/candy441/model.json';
 
 
 function preload() {
